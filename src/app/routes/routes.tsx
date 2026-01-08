0b6127e0313dfa8d3b0e87614e4d6d5889b85d83
k.babichev2015 <k.babichev2015@yandex.ru>
2026-01-08 18:24:25 +0700

    4-tools

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router'
 import { Layout } from '../layout/Layout'
-import { HomePage } from '../../pages'
+import { HomePage } from '@/pages/home'
 
 export const router = createBrowserRouter([
     {
--- src/shared/index.ts
+++
@@ -1,3 +0,0 @@
-export { type EnvConfig, CONFIG } from './config'
-export { http } from './api'
-export * from './ui'
